8550f5a76f8cf7ab49ddebb3a49f9defc0d8473d
Tulas2411 <tuannmhe181587@fpt.edu.vn>
2024-10-05 20:59:02 +0700

    render HTML

---
+++ src/slot2/renderHTML/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+    return (
+        <div>
+            <button />
+            <code />
+            <input />
+            <label/>
+            <p/>
+            <pre/>
+            <select/>
+            <table></table>
+            <ul></ul>
+        </div>
+    )
+}
+export default App;
